e007498ec8ab90d0f04b22cea8e50e28b61457ae
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-11-23 10:31:04 -0600

    login finished

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,138 @@
+var mongodb = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var express = require('express');
 var mongoose = require ('mongoose');
-var mongo = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
+
+//password utils
+var genRandomString = function(length){
+  return crypto.randomBytes(Math.ceil(length/2))
+  .toString('hex') //convert to hexa format
+  .slice(0,length);
+}
+
+var sha512 = function(password, salt){
+  var hash = crypto.createHmac('sha512', salt);
+  hash.update(password, salt);
+  var value = hash.digest('hex');
+  return{
+    salt:salt,
+    passwordHash:value
+  };
+};
+
+function saltHashPassword(userPassword){
+    var salt = genRandomString(16); 
+    var passwordData = sha512(userPassword, salt);
+    return passwordData;
+}
+
+function checkHashPassword(userPassword,salt){
+  var passwordData = sha512(userPassword,salt);
+  return passwordData;
+}
+
+//Create express server 
 var app = express();
-
-var url = 'mongodb://localhost:27017/chat'
-//Conect to DB
-mongoose.connect(url);
-//Express
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-//Routes
-app.use('/api', require('./routes/api'));
+//Create mongoDB client
+var MongoClient = mongodb.MongoClient;
 
-//start server
-app.listen(3000);
-console.log('Server running on port 3000');
+//Connection to mongodb
+var url = 'mongodb://localhost:27017'
 
-/*
+MongoClient.connect(url,{useNewUrlParser:true}, function(err,client){
+  if(err)
+      console.log('Unable to connect.', err);
+  else{
+    //Register part
+    app.post('/register', (request, response, next)=>{
+      var post_data = request.body;
+
+      var plaint_password = post_data.password;
+      var hash_data = saltHashPassword(plaint_password);
+
+      var password = hash_data.passwordHash; //save password
+      var salt = hash_data.salt; // save salt
+
+      var name = post_data.name;
+      var email = post_data.email;
+
+      var insertJson = {
+        'email': email,
+        'password': password,
+        'salt': salt,
+        'name': name
+      };
+      var db = client.db('DBchat');
+
+      //Verify if exists email
+      db.collection('user')
+        .find({'email':email}).count(function(err,number){
+          if(number != 0)
+          {
+            response.json('Email exists');
+            console.log('Email exists');
+          }
+          else{
+            //Insert data
+            db.collection('user')
+                .insertOne(insertJson, function(error, res){
+                  response.json('Registration success');
+                  console.log('Registration success');
+                })
+          }
+        })
+
+    });
+    app.post('/login', (request, response, next)=>{
+      var post_data = request.body;
+
+      var email = post_data.email;
+      var userPassword = post_data.password;
+
+      var db = client.db('DBchat');
+
+      //Verify if exists email
+      db.collection('user')
+        .find({'email':email}).count(function(err,number){
+          if(number == 0)
+          {
+            response.json('Email not exists');
+            console.log('Email not exists');
+          }
+          else{
+            //Insert data
+            db.collection('user')
+                .findOne({'email':email}, function(err,user){
+                  var salt = user.salt;
+                  var hashed_password = checkHashPassword(userPassword, salt).passwordHash;
+                  var encrypted_password = user.password;
+                  if(hashed_password == encrypted_password){
+                    response.json('Login success');
+                    console.log('Login success');
+                  }
+                  else{
+                    response.json('Wrong password');
+                    console.log('Wrong password');
+                  }
+                })
+          }
+        })
+
+    });
+
+    //Start server
+    app.listen(3000,()=>{
+      console.log('Connected on port 3000');
+    })
+  }
+})
+
+
 
 
 /*var express = require('express');
--- Backend/models/users.js
+++
@@ -1,9 +0,0 @@
-var restful = require('node-restful');
-var mongoose = restful.mongoose;
-
-var userSchema = new mongoose.Schema({
-    email: String,
-    userName: String,
-    password: String
-})
-module.exports = restful.model('users', userSchema);
